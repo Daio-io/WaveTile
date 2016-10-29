7feca780a9e60259ab98139a18f58a442bdabcb5
Daio-io <dai.williams@bbc.co.uk>
2016-10-29 17:22:10 +0100

    create base surf tile create wind tile

---
+++ app/src/main/java/io/daio/wavetile/tile/BaseSurfTileService.kt
@@ -0,0 +1,57 @@
+package io.daio.wavetile.tile
+
+import android.service.quicksettings.TileService
+import io.daio.wavetile.Constants
+import io.daio.wavetile.MainActivity
+import io.daio.wavetile.api.beach.BeachFinderAPI
+import io.daio.wavetile.api.beach.BeachStore
+import io.daio.wavetile.api.model.Beach
+import io.daio.wavetile.api.model.SurfData
+import io.daio.wavetile.api.surf.SurfQueryAPI
+import io.daio.wavetile.extensions.removeLastWord
+import io.daio.wavetile.repo.BeachRepo
+import java.util.*
+
+abstract class BaseSurfTileService: TileService() {
+
+    private val api = SurfQueryAPI(Constants.API_KEY)
+    private var repo: BeachRepo? = null
+
+    override fun onClick() {
+        super.onClick()
+        request()
+        MainActivity.start(applicationContext)
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+        val store = BeachStore(applicationContext)
+        repo = BeachRepo(store, BeachFinderAPI(store))
+    }
+
+    override fun onStartListening() {
+        super.onStartListening()
+        request()
+    }
+
+    abstract fun updateTile(beachName: String?, surfData: SurfData?)
+
+    private fun request() {
+        val hour = Calendar.getInstance().get(Calendar.HOUR_OF_DAY)
+        val beach = repo?.getUserSavedBeach()
+
+        if (beach is Beach && beach.id is Int) {
+            api.next(beach.id, hour, {
+                val surfData = it?.response?.first()
+                val name = beach.name.removeLastWord(20)
+
+                updateTile(name, surfData)
+            })
+        } else {
+            qsTile.label = "Not set"
+            qsTile.updateTile()
+        }
+
+    }
+
+}
--- app/src/main/java/io/daio/wavetile/tile/WindTileService.kt
+++ app/src/main/java/io/daio/wavetile/tile/WindTileService.kt
@@ -1,59 +1,13 @@
 package io.daio.wavetile.tile
 
-import android.service.quicksettings.TileService
-import io.daio.wavetile.Constants
-import io.daio.wavetile.MainActivity
-import io.daio.wavetile.api.beach.BeachFinderAPI
-import io.daio.wavetile.api.beach.BeachStore
-import io.daio.wavetile.api.model.Beach
-import io.daio.wavetile.api.surf.SurfQueryAPI
-import io.daio.wavetile.extensions.removeLastWord
-import io.daio.wavetile.repo.BeachRepo
-import java.util.*
+import io.daio.wavetile.api.model.SurfData
 
-class WindTileService: TileService() {
+class WindTileService: BaseSurfTileService() {
 
-    private val api = SurfQueryAPI(Constants.API_KEY)
-    private var repo: BeachRepo? = null
+    override fun updateTile(beachName: String?, surfData: SurfData?) {
 
-    override fun onClick() {
-        super.onClick()
-        request()
-        MainActivity.start(applicationContext)
-    }
-
-    override fun onCreate() {
-        super.onCreate()
-        val store = BeachStore(applicationContext)
-        repo = BeachRepo(store, BeachFinderAPI(store))
-    }
-
-    override fun onStartListening() {
-        super.onStartListening()
-        request()
-    }
-
-    private fun updateTile(beachName: String?, windSpeed: Int?) {
-        val name = beachName?.removeLastWord(20)
-
-        qsTile.label = "$name\n${windSpeed}mph"
+        qsTile.label = "$beachName\n${surfData?.weather?.wind} mph"
         qsTile.updateTile()
-    }
-
-    private fun request() {
-        val hour = Calendar.getInstance().get(Calendar.HOUR_OF_DAY)
-        val beach = repo?.getUserSavedBeach()
-
-        if (beach is Beach && beach.id is Int) {
-            api.next(beach.id, hour, {
-                val wind = it?.response?.first()?.weather?.wind
-                updateTile(beach.name, wind)
-            })
-        } else {
-            qsTile.label = "Not set"
-            qsTile.updateTile()
-        }
-
     }
 
 }
